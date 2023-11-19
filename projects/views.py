2089d80e41905a5b4c8f5dc2da1fd30c8f42f380
Jagarut <jagaruc@gmail.com>
2023-11-19 16:17:04 +0000

    Database queries with ORM

--- projects/views.py
+++ projects/views.py
@@ -1,37 +1,21 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'Site to build out porfolio'
-    },
-    {
-        'id': '3',
-        'title': 'Pets Website',
-        'description': 'Site to show my beautiful pets'
-    },
-]
+from .models import Project, Review
 
 def projects(request):
-    msg = "projects page!"
+    projects = Project.objects.all()
+    
     context = {
-        'message': msg, 
-        'number': 10,
-        'projects': projectsList
+        'projects': projects
     }
 
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    projectObj = None
-    for project in projectsList:
-        if project['id'] == pk:
-            projectObj = project
-    return render(request, 'projects/single-project.html', {'project':projectObj})
+    projectObj = Project.objects.get(id=pk)
+    # tags = projectObj.tags.all()
+
+    context = {
+        'project':projectObj,
+        # 'tags': tags,
+    }
+    return render(request, 'projects/single-project.html', context=context)
